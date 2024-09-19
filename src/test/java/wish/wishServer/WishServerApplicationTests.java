6cf770e7ff467425c58145bc5b19bd5862feebd8
ajeong <ajung7038@naver.com>
2024-09-19 10:47:09 +0900

    Feat: 기본 세팅 #1

---
+++ src/test/java/wish/wishServer/WishServerApplicationTests.java
@@ -0,0 +1,13 @@
+package wish.wishServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
